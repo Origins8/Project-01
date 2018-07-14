565735e389f38f1e86e4d5aaf6defa99bd8db5d5
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-07-14 21:57:59 +0530

    DB path added

--- src/project/pkg01/Project01.java
+++ src/project/pkg01/Project01.java
@@ -13,15 +13,12 @@ public class Project01 {
         try{
             //Class.forName("com.mysql.jdbc.Driver");
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentmanagementsystem?zeroDateTimeBehavior=convertToNull","root",""); //Make Connection to the Database
-            System.out.println("Connected");
             stmt = conn.createStatement();            
           }catch(SQLException e){
             System.err.println(e);
             e.printStackTrace();
           }
-    }
-
-        
+    }     
     public static void main(String[] args) {
        
     
