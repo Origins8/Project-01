eb8f607dc1712402e24125af356327da0aaf5bab
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-07-14 21:26:33 +0530

    DB Connection code added

--- src/project/pkg01/Project01.java
+++ src/project/pkg01/Project01.java
@@ -1,9 +1,30 @@
 package project.pkg01;
 
+import java.sql.*;
+import java.util.Scanner;
+
 public class Project01 {
 
-    public static void main(String[] args) {
+       private Connection conn;
+       private Statement stmt;
+       private ResultSet rs;
+
+    public Project01() {
+        try{
+            //Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentmanagementsystem?zeroDateTimeBehavior=convertToNull","root",""); //Make Connection to the Database
+            System.out.println("Connected");
+            stmt = conn.createStatement();            
+          }catch(SQLException e){
+            System.err.println(e);
+            e.printStackTrace();
+          }
+    }
+
         
+    public static void main(String[] args) {
+       
+    
     }
     
 }
