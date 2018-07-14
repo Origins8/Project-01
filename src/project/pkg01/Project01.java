7af5d6cbf2dcd570ffc522e8439142d6dbb6dc65
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-07-14 22:13:36 +0530

    Full code added without included the codes to share with db

--- src/project/pkg01/Project01.java
+++ src/project/pkg01/Project01.java
@@ -29,6 +29,67 @@ public class Project01 {
         boolean wrongDecision = false; //User Select wrong option
         
         System.out.println("|||||||||||||||WELCOME TO STUDENT MANAGEMENT SYSTEM|||||||||||||||");
+        
+         while(Continuation == true){
+            
+           if(WrongChoice == false){
+                
+                System.out.println("\nWhich operation do you want to do?\n\t1-Add new Student\n\t2-Get all Student informations\n\t3-Get one Student information\n");
+                int UserChoice = objUser.nextInt(); //Get the input from the user 
+
+                switch (UserChoice) {
+                    case 1:
+                        System.out.print("\nEnter Student ID       : ");
+                        String id = objUser.next();
+                        System.out.print("Enter Student name      : ");
+                        String name = objUser.next();
+                        System.out.print("Enter Student Age       : ");
+                        String age = objUser.next();
+                        System.out.print("Enter Student address   : ");
+                        String address = objUser.next();
+                        
+                        String insert = "INSERT INTO student(ID, NAME, AGE, ADDRESS)" + "VALUES (?,?,?,?)";
+                
+                        break;
+                    case 2:
+                        
+                        String all = "SELECT * FROM `student`";
+
+                        break;
+                    case 3:
+                        
+                        System.out.println("Enter Student ID        : ");
+                        id = objUser.next();
+                        String personal = "SELECT * FROM `student` WHERE ID=id";
+                
+                        break;
+                    default:
+                        System.out.println("Sorry your choice of action is incorrect. Please try again!");
+                        WrongChoice = true;
+                        break;
+                }
+           }  
+            
+            System.out.println("\nDo You want to continue? y-yes / n-No");
+            decision = objUser.next();
+            
+            
+            while(wrongDecision == true){
+             switch (decision) {
+               case "y":
+                   Continuation = true;
+                   wrongDecision = false;
+                   break;
+               case "n":
+                   Continuation = false;
+                   wrongDecision = false;
+                   break;
+               default:
+                   System.out.println("Please enter the correct letter");
+                   wrongDecision = true;
+                   break;
+            }   
+          }
+       }  
     }
-    
 }
